5467ca58095c94b0f8a454e5521a514ec4f49ebb
Suman-89 <suman.mukherjee01@gmail.com>
2023-06-14 20:37:55 +0530

    edit

--- src/app_using_jsonserver/employeeCrud/EmployeeEdit.js
+++ src/app_using_jsonserver/employeeCrud/EmployeeEdit.js
@@ -56,7 +56,7 @@ const EmployeeEdit = () => {
                   setEmployeeEditState({
                     ...employeeEditState,
                     empName: e.target.value,
-                  })
+                  }) 
                 }
               />
             </Form.Group>
